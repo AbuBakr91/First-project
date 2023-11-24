c66bcd9992dcf649c4470c3d0f200ee0bd96c14d
AbuBakr91 <bulchaev91@mail.ru>
2023-11-24 10:54:26 +0300

    DEV-1 зарегистрировал класс и интерфейс

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\EmployeeInterface;
+use App\Services\EmployeeService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(EmployeeInterface::class, EmployeeService::class);
+
     }
 
     /**
